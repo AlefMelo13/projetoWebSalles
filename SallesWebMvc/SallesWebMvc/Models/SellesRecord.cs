df9c8a6fdff901331e187a881f31efd09e619d38
AlefMelo <alefmelo13@gmail.com>
2022-08-17 13:46:49 -0300

    criadas novas entidades de negócio e adicionados os dbsets

---
+++ SallesWebMvc/SallesWebMvc/Models/SellesRecord.cs
@@ -0,0 +1,26 @@
+﻿using SallesWebMvc.Models.Enums;
+
+namespace SallesWebMvc.Models
+{
+    public class SellesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SellesRecord()
+        {
+        }
+
+        public SellesRecord(int id, DateTime date, double amount, SalleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
