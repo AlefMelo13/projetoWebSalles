eb0b4592f11e9f6940cb8e7f68ca30d5bd88db5f
AlefMelo <alefmelo13@gmail.com>
2022-08-19 15:16:45 -0300

    validação de campos e centralização de navbar

--- SallesWebMvc/SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/SallesWebMvc/Controllers/SellersController.cs
@@ -35,19 +35,26 @@ namespace SallesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Delete(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided!" });
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
+            if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not found!" });
             }
@@ -82,19 +89,19 @@ namespace SallesWebMvc.Controllers
         //Método GET do Edit
         public IActionResult Edit(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided!" });
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
+            if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not found!" });
             }
 
             List<Department> departments = _departmentService.FindAll();
-            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
 
             return View(viewModel);
         }
@@ -103,7 +110,14 @@ namespace SallesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch!" });
             }
--- SallesWebMvc/SallesWebMvc/Models/Seller.cs
+++ SallesWebMvc/SallesWebMvc/Models/Seller.cs
@@ -7,16 +7,22 @@ namespace SallesWebMvc.Models
         public int Id { get; set; }
 
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Informe o Nome")]
+        [StringLength(60, MinimumLength = 6, ErrorMessage = "Tamanho deve ser entre {2} e {1}")]
         public string Name { get; set; }
 
         [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Informe o E-mail")]
+        [EmailAddress(ErrorMessage = "Informe um E-mail válido")]
         public string Email { get; set; }
 
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Informe a Data de Nascimento")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Salário Base")]
+        [Required(ErrorMessage = "Informe o Salário Base")]
         public decimal BaseSalary { get; set; }
         public Department? Department { get; set; }
         public int DepartmentId { get; set; }
