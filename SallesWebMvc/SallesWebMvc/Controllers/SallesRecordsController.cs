adb137daf368c132049f2afd91454becf8342de5
AlefMelo <alefmelo13@gmail.com>
2022-08-22 16:52:05 -0300

    contiuação de implantação de consulta de vendas

--- SallesWebMvc/SallesWebMvc/Controllers/SallesRecordsController.cs
+++ SallesWebMvc/SallesWebMvc/Controllers/SallesRecordsController.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Services;
 
 namespace SallesWebMvc.Controllers
 {
     public class SallesRecordsController : Controller
     {
+        private readonly SallesRecordService _sallesRecordService;
+        public SallesRecordsController(SallesRecordService recordService)
+        {
+            _sallesRecordService = recordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? dataInicial, DateTime? dataFinal)
         {
-            return View();
+            var resultado = await _sallesRecordService.FindByDateAsync(dataInicial, dataFinal);
+            return View(resultado);
         }
 
         public IActionResult GroupingSearch()
---
+++ SallesWebMvc/SallesWebMvc/Services/SallesRecordService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SallesWebMvc.Data;
+using SallesWebMvc.Models;
+
+namespace SallesWebMvc.Services
+{
+    public class SallesRecordService
+    {
+        private readonly SallesWebMvcContext _context;
+
+        public SallesRecordService(SallesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SellesRecord>> FindByDateAsync(DateTime ? dataInicial, DateTime? dataFinal)
+        {
+            var resultado = from obj in _context.SellesRecord select obj;
+            if (dataInicial.HasValue)
+            {
+                resultado = resultado.Where(x => x.Date >= dataInicial.Value);
+            }
+            if (dataFinal.HasValue)
+            {
+                resultado = resultado.Where(x => x.Date <= dataFinal.Value);
+            }
+
+            return await resultado.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
+        }
+    }
+}
